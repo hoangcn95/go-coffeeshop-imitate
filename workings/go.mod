3629e3b8404ed8607a472b87da5917344d3577d2
Peter <petnguyen@axon.com>
2023-06-25 11:25:22 +0700

    feat: adding grpc product client

--- workings/go.mod
+++ workings/go.mod
@@ -5,20 +5,31 @@ go 1.19
 require (
 	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/google/uuid v1.3.0
+	github.com/google/wire v0.5.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0
+	github.com/ilyakaznacheev/cleanenv v1.4.2
+	github.com/pkg/errors v0.9.1
+	github.com/samber/lo v1.38.1
 	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc
 	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230526203410-71b5a4ffd15e // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- workings/internal/counter/infras/grpc/product_client.go
+++ workings/internal/counter/infras/grpc/product_client.go
@@ -1,1 +1,75 @@
 package grpc
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/google/wire"
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/hoangcn95/go-coffeeshop-imitate/workings/cmd/counter/config"
+	"github.com/hoangcn95/go-coffeeshop-imitate/workings/internal/counter/domain"
+	shared "github.com/hoangcn95/go-coffeeshop-imitate/workings/internal/pkg/shared_kernel"
+	"github.com/hoangcn95/go-coffeeshop-imitate/workings/proto/gen"
+)
+
+type productGRPCClient struct {
+	conn *grpc.ClientConn
+}
+
+var _ ProductDomainService = (*productGRPCClient)(nil)
+
+// definition custom dependency injection stuff
+var ProductGRPCClientSet = wire.NewSet(NewGRPCProductClient)
+
+func NewGRPCProductClient(cfg *config.Config) (ProductDomainService, error) {
+	conn, err := grpc.Dial(cfg.ProductClient.URL, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &productGRPCClient{
+		conn: conn,
+	}, nil
+}
+
+func (p *productGRPCClient) GetItemsByType(
+	ctx context.Context,
+	model *domain.PlaceOrderModel,
+	isBarista bool,
+) ([]*domain.ItemModel, error) {
+	c := gen.NewProductServiceClient(p.conn)
+
+	itemTypes := ""
+	if isBarista {
+		itemTypes = lo.Reduce(model.BaristaItems, func(agg string, item *domain.OrderItemModel, _ int) string {
+			return fmt.Sprintf("%s,%s", agg, item.ItemType.String())
+		}, "")
+	} else {
+		itemTypes = lo.Reduce(model.KitchenItems, func(agg string, item *domain.OrderItemModel, _ int) string {
+			return fmt.Sprintf("%s,%s", agg, item.ItemType.String())
+		}, "")
+	}
+
+	req := &gen.GetItemsByTypeRequest{
+		ItemTypes: strings.TrimLeft(itemTypes, ","),
+	}
+	res, err := c.GetItemsByType(ctx, req)
+	if err != nil {
+		return nil, errors.Wrap(err, "productGRPCClient-c.GetItemsByType")
+	}
+
+	results := make([]*domain.ItemModel, 0)
+	for _, item := range res.Items {
+		results = append(results, &domain.ItemModel{
+			ItemType: shared.ItemType(item.Type),
+			Price:    item.Price,
+		})
+	}
+
+	return results, nil
+}
